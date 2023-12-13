7681536a16a23291b7f57d05a724356183609400
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-14 02:10:59 +0300

    Initial Commit

---
+++ CodeSlicingBootcamp/CodeSlicingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeSlicingBootcampApp.swift
+//  CodeSlicingBootcamp
+//
+//  Created by Artem on 14.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeSlicingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
